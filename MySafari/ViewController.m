c810d03cebbfce012a354e82d829834b5eedc625
akalke <amaeya.kalke@gmail.com>
2014-10-02 04:45:51 -0500

    Fixed back/forward button hiding and showing properly. Added helper methods to clean up code

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,11 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIScrollViewDelegate>
 @property (strong, nonatomic) IBOutlet UIWebView *webView;
 @property (strong, nonatomic) IBOutlet UITextField *urlTextField;
 @property (strong, nonatomic) IBOutlet UIButton *backButton;
 @property (strong, nonatomic) IBOutlet UIButton *forwardButton;
+@property (strong, nonatomic) IBOutlet UILabel *pageName;
 
 @end
 
@@ -21,17 +22,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.urlTextField becomeFirstResponder];
+    [self setDefaultPlaceholderURLText];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 #pragma navigation bar
 -(BOOL)textFieldShouldReturn:(UITextField *)textField{
-    NSString *urlString = textField.text;
-    NSLog(@"%@", urlString);
+    [self checkURLString];
+    return YES;
+}
 
-    //Check URL string
-    if([textField.text hasPrefix:@"http://"]){
+- (IBAction)onClearButtonPressed:(id)sender {
+    self.urlTextField.text = @"";
+}
+
+//INCOMPLETE
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+
+}
+
+#pragma topnav helper methods
+-(void)setDefaultPlaceholderURLText{
+    self.urlTextField.placeholder = @"Please enter a URL";
+}
+
+-(void)displayURL{
+    NSURL *url = self.webView.request.URL;
+    NSString *urlString = url.absoluteString;
+    self.urlTextField.text = urlString;
+}
+
+-(void)displayPageName{
+    NSString *webName = [self.webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    self.pageName.text = webName;
+}
+
+-(void)checkURLString{
+    NSString *urlString = self.urlTextField.text;
+    if([self.urlTextField.text hasPrefix:@"http://"]){
         urlString = urlString;
     }
     else{
@@ -39,49 +68,49 @@
         urlString = [NSString stringWithFormat:@"http://%@",incompleteURL];
         self.urlTextField.text = urlString;
     }
-
     NSURL *url = [NSURL URLWithString: urlString];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL: url];
     [self.webView loadRequest:urlRequest];
-
-    return YES;
-
 }
 
-
 # pragma status indicator
-
-//COMPLETE
 - (void)webViewDidStartLoad:(UIWebView *)webView{
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
 }
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView{
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self displayURL];
+    [self displayPageName];
+    [self showHideNavButtons];
 }
 
 
 #pragma navigation
-//INCOMPLETE
 - (IBAction)onBackButtonPressed:(id)sender {
+    [self.webView goBack];
+}
+
+- (IBAction)onForwardButtonPressed:(id)sender {
+    [self.webView goForward];
+}
+
+-(void)showHideNavButtons{
+    //This method shows and hides the back and forward navigation items depending on if the browser has the ability to go back/forward a page.
     if(self.webView.canGoBack == YES){
-        [self.webView goBack];
+        [self.backButton setTitle:@"<" forState:UIControlStateNormal];
     }
     else{
         [self.backButton setTitle:@"" forState:UIControlStateNormal];
     }
-}
 
-//INCOMPLETE
-- (IBAction)onForwardButtonPressed:(id)sender {
     if(self.webView.canGoForward == YES){
-        [self.webView goForward];
+        [self.forwardButton setTitle:@">" forState:UIControlStateNormal];
     }
     else{
         [self.forwardButton setTitle:@"" forState:UIControlStateNormal];
     }
 }
-
 
 //Complete
 - (IBAction)onStopLoadingButtonPressed:(id)sender{
@@ -94,7 +123,6 @@
 }
 
 
-//Complete
 - (IBAction)onPlusButtonPressed:(id)sender {
     UIAlertView *alertView = [[UIAlertView alloc] init];
     alertView.delegate = self;
@@ -103,6 +131,9 @@
     [alertView show];
 
 }
+
+
+
 
 //- (void)didReceiveMemoryWarning {
 //    [super didReceiveMemoryWarning];
